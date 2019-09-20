abe1d304eed75c8121ef37f1ef12ffa350cb0751
syadoziku <syadoziku@icloud.com>
2019-09-20 08:20:46 +0000

    tasksresourses

---
+++ db/migrate/20190920074914_create_tasks.rb
@@ -0,0 +1,13 @@
+class CreateTasks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.date :worked_on
+      t.string :note
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+    add_index :tasks, [:user_id, :created_at]
+  end
+end
